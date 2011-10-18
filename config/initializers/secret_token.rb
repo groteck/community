5a5965849a61d6671b8a3cc121e925a1cb79a346
Juan Fraire <groteckgmail.com>
2011-10-18 20:34:18 +0100

    modeling users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Community::Application.config.secret_token = 'd1be9f32bfc7379ef3f263fd48fab64763bfb0c9924b23e87f79f4a1d4276f3e2abbcbddc2ef05620156a5b788ecf47cb3d22af142a19aabcad93709c64d5210'
