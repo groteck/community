c0c37d93d7a28ab0503fe7a1fe7e383ca15856e1
Juan Fraire <groteckgmail.com>
2011-11-15 03:21:11 +0000

    añadido ckeditor falta arreglar edicion de comment, y formatear correctamente vistas de entries

--- Gemfile
+++ Gemfile
@@ -6,8 +6,8 @@ gem 'rails', '3.1.1'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
-
+gem "ckeditor", :git => 'git://github.com/fxposter/ckeditor.git'
+gem 'paperclip'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111030015950) do
+ActiveRecord::Schema.define(:version => 20111114212100) do
 
   create_table "blog_entries", :force => true do |t|
     t.string   "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(:version => 20111030015950) do
     t.integer  "user_id"
     t.datetime "updated_at"
     t.datetime "created_at"
-    t.integer  "preview", :default => 1500, :null => false
+    t.integer  "preview",    :default => 1500
   end
 
   create_table "blog_entries_tags", :force => true do |t|
@@ -30,9 +30,22 @@ ActiveRecord::Schema.define(:version => 20111030015950) do
     t.datetime "updated_at"
   end
 
+  create_table "ckeditor_assets", :force => true do |t|
+    t.string   "data_file_name",                  :null => false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    :limit => 30
+    t.string   "type",              :limit => 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+
   create_table "comments", :force => true do |t|
     t.text     "content"
-    t.datetime "update_at"
     t.integer  "user_id"
     t.integer  "blog_entry_id"
     t.datetime "created_at"
@@ -48,7 +61,7 @@ ActiveRecord::Schema.define(:version => 20111030015950) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.integer  "access_level", :default => 0, :null => false
+    t.integer  "access_level",    :default => 0, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
